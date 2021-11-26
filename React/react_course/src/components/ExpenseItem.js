7bb2f09864f0ae84f7d0e85f6d34208a4c229e79
Nishank Yadav <Nishank.Yadav@kyndryl.com>
2021-11-26 09:35:16 +0530

    Creating components and breaking down main app.js into by creating small components

--- React/react_course/src/components/ExpenseItem.js
+++ React/react_course/src/components/ExpenseItem.js
@@ -1,15 +1,19 @@
 import './ExpenseItem.css'
+import ExpenseDate from './ExpenseDate';
+import Card from './Card';
 
 function ExpenseItem (props) {
     
     return (
-    <div className="expense-item">
-        <div>{props.date.toISOString()}</div>
-        <div className="expense-item_description">
-            <h2>{props.title}</h2>
-            <div className="expense-item_price">${props.amount}</div>
-        </div>
-    </div>
+    <Card className="expense-item">
+        <ExpenseDate date={props.date} />
+            <div className="expense-item_description">
+                <h2>{props.title}</h2>
+                <div className="expense-item_price">${props.amount}</div>
+            </div>
+    </Card>
+       
+    
     )
 }
 
