d94c6578d4873df92b03fb6f3cbec2d0de9dddb7
unknown <radhika.makkar@impactqa.com>
2021-12-28 16:59:55 +0530

    Revert "Assignment for React Compresser"
    This reverts commit 3d4c8f850badcd8e523f4ec978fba7b1642c8171.

--- Task-2/src/App.js
+++ Task-2/src/App.js
@@ -3,61 +3,55 @@ import React from 'react';
 import NewExpense from './components/NewExpense/NewExpense';
 import Expenses from './components/Expenses/Expenses';
 import { useState } from 'react/cjs/react.development';
-import Practice from "./Practice";
 
-  // const Dummy_Data = [
-  //   {
-  //     id: 'e1',
-  //     title: 'Toilet Paper',
-  //     amount: 94.12,
-  //     date: new Date(2020, 7, 14),
-  //   },
-  //   { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
-  //   {
-  //     id: 'e3',
-  //     title: 'Car Insurance',
-  //     amount: 294.67,
-  //     date: new Date(2021, 2, 28),
-  //   },
-  //   {
-  //     id: 'e4',
-  //     title: 'New Desk (Wooden)',
-  //     amount: 450,
-  //     date: new Date(2021, 5, 12),
-  //   },
-  // ];
 
-  // const App = () => {
-  //   const [expenses, setExpenses] = useState(Dummy_Data);
+  const Dummy_Data = [
+    {
+      id: 'e1',
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: 'e3',
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: 'e4',
+      title: 'New Desk (Wooden)',
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
+  ];
 
-  //   const addExpenseHandler = (expense) => {
-  //     setExpenses((prevExpenses) => {
-  //       return [expense, ...prevExpenses]
-  //     })
-  //   };
+  const App = () => {
+    const [expenses, setExpenses] = useState(Dummy_Data);
+
+    const addExpenseHandler = (expense) => {
+      setExpenses((prevExpenses) => {
+        return [expense, ...prevExpenses]
+      })
+    };
   
-  //   // return React.createElement(
-  //   //   'div',
-  //   //   {},
-  //   //   React.createElement('h2', {}, "Let's get started!"),
-  //   //   React.createElement(Expenses, { items: expenses })
-  //   // );
+    // return React.createElement(
+    //   'div',
+    //   {},
+    //   React.createElement('h2', {}, "Let's get started!"),
+    //   React.createElement(Expenses, { items: expenses })
+    // );
   
-  //   return (
-  //     <div>
-  //       <NewExpense onAddExpense={addExpenseHandler} />
-  //       <Expenses items={expenses} />
-  //     </div>
-  //   // );
-  // }
-
-  function App(){
-    return(
-     <div>
-       <Practice/>
-     </div>
-    )
+    return (
+      <div>
+        <NewExpense onAddExpense={addExpenseHandler} />
+        <Expenses items={expenses} />
+      </div>
+    );
   }
+
+  
 
 
 export default App;
