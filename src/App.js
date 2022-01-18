607354c9fd3a459e7c8054052dae9698be0e700d
Tarun Abijith <kabijith147@gmail.com>
2022-01-18 21:28:20 +0530

    Generator-function

--- src/App.js
+++ src/App.js
@@ -2,22 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  async function *  getData(){
+  const response=await fetch("https://restcountries.com/v3.1/all");
+  yield await response.json();
+  }
+const data=getData();
+console.log(data,"Generator Function")
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    Generator Function
     </div>
   );
 }
