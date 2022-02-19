724d44b683c367f79c4fb8a29749f478eac0d427
Nishank Yadav <Nishank.Yadav@kyndryl.com>
2022-02-19 09:49:48 +0530

    React testing

---
+++ React/testing/app_testing/src/components/Greeting.test.js
@@ -0,0 +1,13 @@
+import { render, screen } from "@testing-library/react";
+import Greeting from "./Greetings";
+
+test('renders Hello World as a text', () => {
+    //Arrange
+    render(<Greeting />);
+    //Act
+    //...nothing
+
+    //Assert
+    const helloWorldElement = screen.getByText('Hello World', { exact: false});
+    expect(helloWorldElement).toBeInTheDocument();
+})
