d0519b9a5b8907b071df839a59fc9696d5b236eb
Nishank Yadav <Nishank.Yadav@kyndryl.com>
2022-02-22 16:55:56 +0530

    Testing completed

--- React/testing/app_testing/src/components/Greeting.test.js
+++ React/testing/app_testing/src/components/Greeting.test.js
@@ -1,13 +1,44 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Greeting from "./Greetings";
 
-test('renders Hello World as a text', () => {
-    //Arrange
-    render(<Greeting />);
-    //Act
-    //...nothing
+describe('Greeting component', () => {
+    
+    test('renders Hello World as a text', () => {
+        //Arrange
+        render(<Greeting />);
+        //Act
+        //...nothing
 
-    //Assert
-    const helloWorldElement = screen.getByText('Hello World', { exact: false});
-    expect(helloWorldElement).toBeInTheDocument();
-})
+        //Assert
+        const helloWorldElement = screen.getByText('Hello World', { exact: false});
+        expect(helloWorldElement).toBeInTheDocument();
+    });
+
+    test('renders "Changed!!" if the button was clicked', () =>{
+        //Arrange
+        render(<Greeting />);
+
+        //Act
+        const buttonElement = screen.getByRole('button');
+        userEvent.click(buttonElement)
+
+        //Assert
+        const outputElement = screen.getByText('Changed!!');
+        expect(outputElement).toBeInTheDocument();
+    });
+
+    test('does not render "It\'s good to practice" if the button was clicked', () => {
+        //Arrange
+        render(<Greeting />);
+
+        //Act
+        const buttonElement = screen.getByRole('button');
+        userEvent.click(buttonElement)
+
+        //Assert
+        const outputElement = screen.queryByText('It\'s good to practiceIttesting', {exact: false});
+        expect(outputElement).toBeNull();
+    })
+
+});
--- React/testing/app_testing/src/components/Greetings.js
+++ React/testing/app_testing/src/components/Greetings.js
@@ -1,8 +1,17 @@
+import { useState } from "react";
+
 const Greeting = () => {
+    const [changedText, setChengedText] = useState(false);
+
+    const changeTextHandler = () => {
+        setChengedText(true);
+    };
     return (
         <div>
             <h2>Hello World!</h2>
-            <p>It's good tvo practice testing.</p>
+            {!changedText &&<p>It's good to practice testing.</p>}
+            {changedText && <p>Changed!!</p>}
+            <button onClick={changeTextHandler}>Change Text!</button>
         </div>
     )
 }
